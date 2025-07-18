b06117414b1ea2a42bdb9ad744dbccec2f52d64c
huyluongme <huyluongme.cs@gmail.com>
2025-07-18 23:22:58 +0700

    Longest Substring Without Repeating Characters

---
+++ longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#define CHAR_RANGE 256
+
+/*
+    abcabcbb
+    loop1:
+        max_len = 0
+        start = 0
+        end = 0
+        c = 'a'
+        last_index[c] = -1
+        update last_index[c] = 0
+        window_len = 0 - 0 + 1 = 1
+        -> update max_len = 1
+    loop2:
+        max_len = 1
+        start = 0
+        end = 1
+        c = 'b'
+        last_index[c] = -1
+        update last_index[c] = 1
+        window_len = 1 - 0 + 1 = 2
+        -> update max_len = 2
+    loop3:
+        max_len = 2
+        start = 0
+        end = 2
+        c = 'c'
+        last_index[c] = -1
+        update last_index[c] = 2
+        window_len = 2 - 0 + 1 = 3
+        -> update max_len = 3
+    loop4:
+        max_len = 3
+        start = 0
+        end = 3
+        c = 'a'
+        last_index[c] = 0
+        -> update start = 0 + 1 = 1
+        update last_index[c] = 3
+        window_len = 3 - 1 + 1 = 3
+        -> not update max_len
+    ... 
+*/
+
+int lengthOfLongestSubstring(char* s) {
+    int len = strlen(s);
+    int start = 0, max_len = 0;
+    
+    int last_index[CHAR_RANGE];
+    for(int i = 0; i < CHAR_RANGE; i++) last_index[i] = -1;
+    
+    for(int end = 0; end < len; end++) {
+        unsigned char c = (unsigned char)s[end];
+        if(last_index[c] >= start) {
+            start = last_index[c] + 1;
+        }
+
+        last_index[c] = end;
+
+        int window_len = end - start + 1;
+        if(window_len > max_len)
+            max_len = window_len;
+    }
+
+    return max_len;
+}
+
+int main() {
+    char *s1 = "abcabcbb";
+    printf("\"%s\" -> %d\n", s1, lengthOfLongestSubstring(s1));
+    return 0;
+}
